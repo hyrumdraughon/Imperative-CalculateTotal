04f8d1548523175353335547b6ffdfdf91de359f
Hyrum Draughon <hyrumdraughon@gmail.com>
2021-03-23 10:18:44 -0700

    implemented CalculateTotal function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 export function calculateTotal (items, tax) {
-  // TODO
+  tax = Math.abs(tax)
+
+  let total = 0
+
+  for (let item of items) {
+    total += (item.price + item.price * tax * item.taxable)
+  }
+  
+  return total
 }
